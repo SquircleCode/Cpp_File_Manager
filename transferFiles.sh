addafc3ded7dfb5e4d888cb907ae7f73b41f5dcf
SquircleCode <42237261+SquircleCode@users.noreply.github.com>
2018-09-03 04:59:44 +0000

    made functions distinct
    uploadFile.sh only uploads to GitHub  
    transferFiles.sh can transfer both a file and a folder to the local GitHub repository

--- transferFiles.sh
+++ transferFiles.sh
@@ -4,10 +4,25 @@
 touch ~/cpp_file_manager/FileName.txt
 touch ~/cpp_file_manager/FolderName.txt
 
-#finds the required file. runs a script
-sh ~/cpp_file_manager/findFile.sh
+#File or folder
+echo "Do you want to transfer a file or a folder"
+read Response
 
-#FolderName
+if [ $Response == "file" ]
+then
+	#finds the required file. runs a script	
+	sh ~/cpp_file_manager/findFile.sh
+
+	#FileName
+	# gets the temp name file's path
+	file="./cpp_file_manager/FileName.txt"
+	#reads and stores the value in a variable
+	while IFS= read -r line
+	do
+	       #stores the data into the variable
+		FileName="$line"
+	done <"$file"
+
 	# gets the temp name file's path
 	file="./cpp_file_manager/FolderName.txt"
 	#reads and stores the value in a variable
@@ -16,8 +31,21 @@ sh ~/cpp_file_manager/findFile.sh
 	       #stores the data into the variable
 		FolderName="$line"
 	done <"$file"
+	SOURCE="./$FolderName/$FileName"
 
-SOURCE="./$FolderName"
+elif [ $Response == "folder" ]
+then
+	#gets lab number for the folder name	
+	echo "Enter Lab Number : "
+	read LAB_NO
+	# types the folder name in required format and stores it
+	FolderName="lab$LAB_NO"
+	SOURCE="./$FolderName/"
+
+else
+	echo "Invalid Response. Try again"
+	uf
+fi
+
 DESTINATION="./CS_141_GitHub/CS-141-SEM1"
-
-cp -r "$SOURCE/"* "$DESTINATION"
+cp -r "$SOURCE"* "$DESTINATION"
--- uploadFile.sh
+++ uploadFile.sh
@@ -1,45 +1,11 @@
 #!/bin/bash
 
-#creates Temporary name files
-touch ~/cpp_file_manager/FileName.txt
-touch ~/cpp_file_manager/FolderName.txt
-
-#finds the required file. runs a script
-sh ~/cpp_file_manager/findFile.sh
-
-
-#FileName
-	# gets the temp name file's path
-	file="./cpp_file_manager/FileName.txt"
-	#reads and stores the value in a variable
-	while IFS= read -r line
-	do
-	       #stores the data into the variable
-		FileName="$line"
-	done <"$file"
-
-#FolderName
-	# gets the temp name file's path
-	file="./cpp_file_manager/FolderName.txt"
-	#reads and stores the value in a variable
-	while IFS= read -r line
-	do
-	       #stores the data into the variable
-		FolderName="$line"
-	done <"$file"
-
-
 #Uploads the file to github
-cp ~/$FolderName/$FileName.cpp ~/CS_141_GitHub/CS-141-SEM1/$FileName.cpp
 cd ~/CS_141_GitHub/CS-141-SEM1
 git init
 git add .
-git commit -m "Uploader $FileName using shell script"
+git commit -m "Uploaded using shell script"
 git remote add origin "https://github.com/SquircleCode/CS-141-SEM1"
 git push -u origin master
-
-#deletes the temporary name files
-rm ~/cpp_file_manager/FileName.txt
-rm ~/cpp_file_manager/FolderName.txt
 
 
